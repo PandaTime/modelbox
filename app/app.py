98abb6a74ba6d81ee2f0fed675a88530de7570fb
Kirill Kobylyanskiy <kirill.kobylyanskiy@emlid.com>
2017-11-06 18:56:43 +0300

    app: update routing scheme and add road class
    road class requests mapbox api and returns duration and distance trip

--- app/app.py
+++ app/app.py
@@ -1,11 +1,24 @@
 from flask import Flask
-app = Flask(__name__)
+from roads.roads import Road
 
 
-@app.route("/")
-def main():
-    return "Welcome!"
+class Application:
+
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.add_routing()
+
+    def add_routing(self):
+        self.add_endpoint(endpoint='/get_route/<longitude_from>/<latitude_from>/<longitude_to>/<latitude_to>',
+                          endpoint_name='main', handler=Road.get_duration_and_distance)
+
+    def run(self):
+        self.app.run()
+
+    def add_endpoint(self, endpoint=None, endpoint_name=None, handler=None):
+        self.app.add_url_rule(endpoint, endpoint_name, handler)
 
 
 if __name__ == "__main__":
+    app = Application()
     app.run()
