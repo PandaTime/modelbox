f16c1d0fd8860cbedb2c9b1fe1f4f07fad6c54c7
Kirill Kobylyanskiy <kirill.kobylyanskiy@emlid.com>
2017-11-07 02:07:37 +0300

    database: add crud methods

--- database/database.py
+++ database/database.py
@@ -1,29 +1,39 @@
-import psycopg2
+import sqlalchemy
 
 
 class Database:
 
-    def __init__(self, dbname='postgres', user='postgres', host='localhost', password='pass'):
+    def __init__(self, dbname='postgres', user='postgres', host='localhost', password='pass', port=5432):
+        url = 'postgresql://{}:{}@{}:{}/{}'
+        url = url.format(user, password, host, port, dbname)
+        self.con = sqlalchemy.create_engine(url, client_encoding='utf8')
+        self.meta = sqlalchemy.MetaData(bind=self.con, reflect=True)
 
-        self.dbname = dbname
-        self.user = user
-        self.host = host
-        self.password = password
+    def create_warehouse(self, longitude, latitude, capacity=88, cost_of_service=5000):
+        clause = self.meta.tables['regional_warehouses'].insert().values(
+            longitude=longitude, latitude=latitude, capacity=capacity, cost_of_service=cost_of_service)
+        self.con.execute(clause)
 
-        self.connect()
-        self.cur = self.conn.cursor()
+    def create_city(self, name, longitude, latitude, population, country, port_id, warehouse_id):
+        clause = self.meta.tables['cities'].insert().values(
+            name=name, longitude=longitude, latitude=latitude, population=population,
+            country=country, port_id=port_id, warehouse_id=warehouse_id)
+        self.con.execute(clause)
 
-    def connect(self):
-        try:
-            self.conn = psycopg2.connect("dbname='{}' user='{}' host='{}' password='{}'"
-                                         .format(self.dbname, self.user, self.host, self.password))
-        except ConnectionError:
-            print("Unable to connect to the database")
-            exit(1)
+    def create_port(self, port_id, cost_of_shipping_to_us=400, cost_of_shipping_to_china=500):
+        clause = self.meta.tables['ports'].insert().\
+            values(port_id=port_id, cost_of_shipping_to_us=cost_of_shipping_to_us,
+                   cost_of_shipping_to_china=cost_of_shipping_to_china)
+        self.con.execute(clause)
 
-    def insert_city(self, name, population, longitude, latitude, country, port_id):
-        try:
-            self.cur.execute("INSERT INTO cities (NAME, POPULATION, LONGITUDE, LATTITUDE, COUNTRY, PORT_ID)"
-                    "VALUES (%s, %s, %s, %s, %s, %s)", (name, population, longitude, latitude, country, port_id))
-        except Exception:
-            print("Cannot insert")
+    def create_dealer(self):
+        pass
+
+    def read(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
